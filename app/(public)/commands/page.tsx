287447066ede03a9f1d21b62aa56316dfc129fd7
Athena <athena@parthenon.app>
2024-06-10 20:51:10 -0700

    feat(commands): Added a Commands page and adjusted styles

---
+++ app/(public)/commands/page.tsx
@@ -0,0 +1,51 @@
+import { DiscordIcon, TwitchIcon } from '../../icons';
+import { CommandsDiscord, CommandsTwitch } from '../../lib/constants';
+import styles from './page.module.scss';
+
+const Commands = () => {
+  return (
+    <div className={styles.commands}>
+      <h1 className={styles.title}>COMMANDS</h1>
+      <div className={styles.boxes}>
+        <div className={styles.box}>
+          <h2>
+            <span>TWITCH</span>
+            <TwitchIcon />
+          </h2>
+          <ul className={styles.list}>
+            {CommandsTwitch.map((command, i) => (
+              <li key={i}>
+                <h3>
+                  {command.name}
+                  {command.sub && <span>{command.sub}</span>}
+                </h3>
+                <p>{command.description}</p>
+                {command.note && <p>{command.note}</p>}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.box}>
+          <h2>
+            <span>DISCORD</span>
+            <DiscordIcon />
+          </h2>
+          <ul className={styles.list}>
+            {CommandsDiscord.map((command, i) => (
+              <li key={i}>
+                <h3>
+                  {command.name}
+                  {command.sub && <span>{command.sub}</span>}
+                </h3>
+                <p>{command.description}</p>
+                {command.note && <p>{command.note}</p>}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Commands;
--- app/lib/constants/index.ts
+++ app/lib/constants/index.ts
@@ -1,3 +1,50 @@
+export const CommandsDiscord = [
+  {
+    name: '/8ball',
+    sub: '<value>',
+    description: 'Play a game of Magic 8-Ball',
+    note: 'Accepted Value: <question>',
+  },
+  {
+    name: '/coinflip',
+    description: 'Flip a coin!',
+  },
+  {
+    name: '/gamble',
+    sub: '<value>',
+    description: 'Play your points for a chance to double it',
+    note: 'Accepted Values: <positive integer> | half | all',
+  },
+  {
+    name: '/give',
+    sub: '<@member> <value>',
+    description: 'Give your points to another user',
+    note: 'Accepted Value: <positive integer>',
+  },
+  {
+    name: '/points',
+    description: 'View your current balance',
+  },
+  {
+    name: '/star',
+    sub: '<@member>',
+    description: 'Give a star to a user as a form of endorsement',
+  },
+];
+
+export const CommandsTwitch = [
+  {
+    name: '!gamble',
+    sub: '<value>',
+    description: 'Play your points for a chance to double it',
+    note: 'Accepted Values: <positive integer> | half | all',
+  },
+  {
+    name: '!points',
+    description: 'View your current balance',
+  },
+];
+
 export const NavPaths = [
   {
     label: 'Home',
