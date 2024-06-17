01b4b95430577f024fe515e00d861023305911fa
Athena <athena@parthenon.app>
2024-06-17 02:50:56 -0700

    chore(build): Updated build version

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ const RootLayout = ({
             <main className={styles.main}>
               <div className={styles.content}>{children}</div>
               <footer className={styles.footer}>
-                Made with ♡ by Athena | Build v0.6.0
+                Made with ♡ by Athena | Build v0.7.0
               </footer>
             </main>
           </ParthenonProvider>
