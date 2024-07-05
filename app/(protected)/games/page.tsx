b890b63d2ac07fc23bbe242820042e69e1b34695
Athena <athena@parthenon.app>
2024-07-05 16:49:41 -0700

    refactor(games): Added mobile styles for the page

--- app/(protected)/games/page.tsx
+++ app/(protected)/games/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { WordleIcon } from '../../images/icons';
 import styles from './page.module.scss';
 
 const Games = () => {
@@ -8,10 +9,13 @@ const Games = () => {
     <div className={styles.games}>
       <h1>GAMES</h1>
       <p className={styles.description}>
-        Tired of gambling all and losing? Erase your debts (and more) here!
+        Tired of gambling all and losing?
+        <br />
+        Earn coins by playing some games here!
       </p>
       <div className={styles.grid}>
         <Link className={styles.gridItem} href="/games/wordle">
+          <WordleIcon />
           <span>Wordle</span>
         </Link>
       </div>
