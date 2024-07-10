2e896daa61ce0c5cca956c2af56c0f61ee0a2078
Athena <athena@parthenon.app>
2024-07-10 00:09:45 -0700

    chore(faq): Updated copy and some styles for the FAQ page

--- app/(public)/faq/page.tsx
+++ app/(public)/faq/page.tsx
@@ -10,12 +10,16 @@ const FAQ = () => {
     <div className={styles.faq}>
       <h1>FREQUENTLY ASKED QUESTIONS</h1>
       <div className={styles.questions}>
-        <h2>Can I add the bot to my chat / server?</h2>
+        <h2>Can I add Little Owl to my chat / server?</h2>
         <p>
           Sorry, no. The bot is meant to be used privately within the AthenaUS
           community.
         </p>
         <h2>How can I link my Discord and Twitch accounts?</h2>
+        <p>
+          By default, your accounts are treated separately. To link your
+          accounts and merge all the coins you have:
+        </p>
         <ol>
           <li>Login on this website via Twitch</li>
           <li>Copy the code provided in your profile</li>
@@ -31,9 +35,15 @@ const FAQ = () => {
         <h2>What if I linked the wrong account?</h2>
         <p>
           In the Discord server, use the <code>/unlink</code> command to unlink
-          your accounts.
-          <br />
-          Note: Your coins will stay in your Discord account.
+          your accounts. All of your coins will stay in your Discord account and
+          your Twitch account will not be associated anymore.
+        </p>
+        <h2>Where can I send feedback or suggestions?</h2>
+        <p>
+          There is a dedicated channel in our Discord server named #feedback
+          where you can make suggestions or discuss your concerns. You can also
+          send an email to{' '}
+          <a href="mailto:athena@parthnon.app">athena@parthenon.app</a>.
         </p>
       </div>
     </div>
