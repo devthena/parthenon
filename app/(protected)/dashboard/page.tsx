b1d1fc083363cd41212c4f3623b2562d92197d07
Athena <athena@parthenon.app>
2024-07-18 18:58:43 -0700

    chore(metadata): [OWL-70761218] Added missing titles for some pages

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,88 +1,10 @@
-'use client';
+import { Metadata } from 'next';
 
-import { useUser } from '@auth0/nextjs-auth0/client';
-import Image from 'next/image';
+import Dashboard from './dashboard';
 
-import { Loading } from '@/components';
-import { useParthenonState } from '@/context';
-import { CoinIcon, StarIcon } from '@/images/icons';
-
-import { AccountLinked, Instructions, Register } from './components';
-import styles from './page.module.scss';
-
-const Dashboard = () => {
-  const { user: userAuth0 } = useUser();
-  const { isFetched, activities, user } = useParthenonState();
-
-  let displayName = '';
-
-  if (user) {
-    if (user.discord_name) displayName = `, ${user.discord_name}`;
-    else if (user.discord_username) displayName = `, ${user.discord_username}`;
-    else if (user.twitch_username) displayName = `, ${user.twitch_username}`;
-  }
-
-  const renderRightSection = () => {
-    if (!isFetched) return <Loading />;
-
-    if (!user) {
-      if (isFetched) return <Register />;
-      return;
-    }
-
-    if (user.discord_username) {
-      if (user.twitch_username) {
-        return (
-          <AccountLinked
-            discord={user.discord_username ?? 'Discord'}
-            twitch={user.twitch_username ?? 'Twitch'}
-          />
-        );
-      } else {
-        return <Instructions />;
-      }
-    } else {
-      return user.twitch_username && <Instructions code={user.code} />;
-    }
-  };
-
-  return (
-    <div className={styles.dashboard}>
-      <div className={styles.info}>
-        <h1>Welcome{displayName}!</h1>
-        <div className={styles.bio}>
-          <figure className={styles.avatar}>
-            {userAuth0?.picture && (
-              <Image
-                alt="Avatar"
-                height={200}
-                priority
-                src={userAuth0.picture}
-                width={200}
-              />
-            )}
-          </figure>
-          <div className={styles.balance}>
-            <div className={styles.item}>
-              <p className={styles.label}>
-                <span>POINTS</span>
-                <span>{user ? user.cash : 0}</span>
-              </p>
-              <CoinIcon />
-            </div>
-            <div className={styles.item}>
-              <p className={styles.label}>
-                <span>STARS</span>
-                <span>{activities ? activities.stars : 0}</span>
-              </p>
-              <StarIcon />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.status}>{renderRightSection()}</div>
-    </div>
-  );
+export const metadata: Metadata = {
+  title: 'Parthenon | Dashboard',
 };
 
-export default Dashboard;
+const DashboardPage = () => <Dashboard />;
+export default DashboardPage;
--- app/(protected)/games/page.tsx
+++ app/(protected)/games/page.tsx
@@ -1,38 +1,10 @@
-'use client';
+import { Metadata } from 'next';
 
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
+import Games from './games';
 
-import { Loading } from '@/components';
-import { useParthenonState } from '@/context';
-import { WordleIcon } from '@/images/icons';
-
-import styles from './page.module.scss';
-
-const Games = () => {
-  const { user, isFetched, isLoading } = useParthenonState();
-
-  if (isFetched && !user?.discord_username) redirect('/dashboard');
-  if (isLoading) return <Loading />;
-
-  return (
-    isFetched && (
-      <div className={styles.games}>
-        <h1>GAMES</h1>
-        <p className={styles.description}>
-          Tired of gambling all and losing?
-          <br />
-          Earn coins by playing some games here!
-        </p>
-        <div className={styles.grid}>
-          <Link className={styles.gridItem} href="/games/wordle">
-            <WordleIcon />
-            <span>Wordle</span>
-          </Link>
-        </div>
-      </div>
-    )
-  );
+export const metadata: Metadata = {
+  title: 'Parthenon | Games',
 };
 
-export default Games;
+const GamesPage = () => <Games />;
+export default GamesPage;
