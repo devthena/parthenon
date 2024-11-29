d25c625d62ecfdb9bfbcd4bb682a99252b68dc33
Athena <athena@parthenon.app>
2024-11-29 06:30:51 -0800

    fix(utils): Refactored encryption function to make sure type is valid

--- app/lib/utils/encryption.ts
+++ app/lib/utils/encryption.ts
@@ -2,11 +2,11 @@ import crypto from 'crypto';
 
 const algorithm = 'aes-256-cbc';
 const encryptionKey = process.env.NEXT_PUBLIC_ENCRYPTION_KEY ?? '';
-const key = Buffer.from(encryptionKey, 'hex');
+const key = Uint8Array.from(Buffer.from(encryptionKey, 'hex'));
 
 export const encrypt = (value: string): string => {
   const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const cipher = crypto.createCipheriv(algorithm, key, Uint8Array.from(iv));
 
   let encrypted = cipher.update(value, 'utf8', 'hex');
   encrypted += cipher.final('hex');
@@ -16,7 +16,7 @@ export const encrypt = (value: string): string => {
 
 export const decrypt = (encryptedText: string): string => {
   const [ivHex, encrypted] = encryptedText.split(':');
-  const iv = Buffer.from(ivHex, 'hex');
+  const iv = Uint8Array.from(Buffer.from(ivHex, 'hex'));
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
 
   let decrypted = decipher.update(encrypted, 'hex', 'utf8');
